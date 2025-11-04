558c9b026e632cad4d13d02c4db1571f9160d0c2
Hatim.Hakeel <Hatim.Hakeel@acumatica.com>
2025-11-04 23:37:37 +0530

    Add server port

--- app.py
+++ app.py
@@ -18,8 +18,9 @@ def hello():
 if __name__ == '__main__':
     import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
+    PORT = 8870
+    '''try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
-        PORT = 5555
+        PORT = 5555'''
     app.run(HOST, PORT)
