7d70dc3ae41d82909c10dfc079d73fced95bb7fa
Hatim.Hakeel <Hatim.Hakeel@acumatica.com>
2025-11-09 21:52:51 +0530

    Migrate to new solution-project structure

---
+++ del/app_del.py
@@ -0,0 +1,47 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+from flask import Flask
+from services.tsforecastservice import sales_forecasts_trainer, sales_forecasts
+
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+
+@app.route('/')
+def hello():
+    """Renders a sample page."""
+    return "Hello World!"
+
+@app.route('/sales_forecast/<customerid>/<int:predictinterval>')
+def sales_forecast(customerid, predictinterval):
+    """Returns a json"""
+
+    result = sales_forecasts(customerid, predictinterval)
+    print("Results")
+    print(result)
+    print("Type of result")
+    print(type(result))
+
+    return result
+
+@app.route('/sales_forecast/train/<customerid>')
+def sales_forecast_train(customerid):
+    
+    result = sales_forecasts_trainer(customerid)
+    return result
+
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    PORT = 8870
+    '''try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555'''
+    app.run(HOST, PORT)
